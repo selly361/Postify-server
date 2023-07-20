b8591d3960518fa40bb79d3cc8d6891235ad3d70
seikou <seikou615@gmail.com>
2023-07-20 21:47:29 +0100

    Setted up server and connected to db

---
+++ index.js
@@ -0,0 +1,23 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
+  )
+  .catch(console.log);
