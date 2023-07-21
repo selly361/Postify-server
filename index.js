ee85067ddcb5c817f7c6f88705206302fa9ad7c2
seikou <seikou615@gmail.com>
2023-07-21 21:10:42 +0100

    Refactored cors

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
+import cors from "cors"
+import dotenv from "dotenv"
+import express from "express"
+import mongoose from "mongoose"
 import postsRouter from "./routes/posts.js"
 
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(
+  cors({
+     origin:
+        process.env.NODE_ENV === 'development'
+           ? true
+           : process.env.NODE_ENV === 'production'
+           ? process.env.ORIGIN
+           : false
+  })
+)
+
 
 app.use("/posts", postsRouter)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
@@ -23,4 +34,4 @@ mongoose
   .then(() =>
     app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
   )
-  .catch(console.log);
+  .catch(console.log)
