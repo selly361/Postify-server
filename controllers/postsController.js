26488db307c848843d1ad29d374fa7ae4f2bb014
seikou <seikou615@gmail.com>
2023-07-21 16:49:04 +0100

    Implemented controllers for CRUD operations

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,103 @@
-const getPosts = (req, res) => {
+import postMessage from "../models/postMessage.js"
+
+const getPosts = async (req, res) => {
+  
+  try {
+  
+    const posts = await postMessage.find()
     
+    res.status(200).json(posts)
+  
+  } 
+  
+  catch (error) {
+    
+    res.status(404).json({ message: error.message })
+  
+  }
+
 }
 
-export { getPosts }
+const createPost = async (req, res) => {
+  
+  const post = req.body
+  
+  const newPost = new postMessage(post)
+  
+  try {
+  
+    await newPost.save()
+    
+    res.status(201).json(newPost)
+
+  } 
+  
+  catch (error) {
+    
+    res.status(409).json({ message: error.message })
+  
+  }
+
+}
+
+const updatePost = async (req, res) => {
+  
+  const { id } = req.params
+  
+  try {
+    
+    const updatedPost = await postMessage.findByIdAndUpdate(id, req.body, { new: true })
+  
+    res.status(200).json(updatedPost)
+  
+  } 
+  
+  catch (error) {
+    
+    res.status(409).json({ message: error.message })
+  
+  }
+
+}
+
+const likePost = async (req, res) => {
+  
+  const { id } = req.params
+  
+  try {
+    
+    const post = await postMessage.findById(id)
+  
+    post.likes += 1
+  
+    const updatedPost = await post.save()
+  
+    res.status(200).json(updatedPost)
+  
+  } 
+  
+  catch (error) {
+    res.status(409).json({ message: error.message })
+  }
+
+}
+
+const deletePost = async (req, res) => {
+  
+  const { id } = req.params
+  
+  try {
+    
+    await postMessage.findByIdAndRemove(id)
+  
+    res.status(200).json({ message: 'Post deleted successfully' })
+  
+  } 
+  
+  catch (error) {
+    res.status(409).json({ message: error.message })
+  }
+
+}
+
+export { getPosts, createPost, updatePost, likePost, deletePost }
