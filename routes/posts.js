ae9b6e5959d53015bb8e1b84250b335985e6d8a4
seikou <seikou615@gmail.com>
2023-07-21 17:07:00 +0100

    Used controllers in their corresponding routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,17 @@
+import * as controllers from "../controllers/postsController"
+
 import express from "express"
-import { getPosts } from "../controllers/postsController"
 
 const router = express.Router()
 
+router.get("/", controllers.getPosts)
 
-router.use("/", getPosts)
+router.post("/", controllers.createPost)
 
+router.patch("/:id", controllers.updatePost)
 
+router.patch("/likePost/:id", controllers.likePost)
+
+router.delete("/:id", controllers.deletePost)
 
 export default router
